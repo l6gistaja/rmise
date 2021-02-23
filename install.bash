5da46fee89e3be42dcb0ce9371103a21a2069dd5
Juks <juks@alkohol.ee>
2021-02-23 21:59:58 +0200

    installer changes

--- install.bash
+++ install.bash
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#git submodule add https://github.com/Laradock/laradock.git
-cd laradock
-#cp env-example .env
+cd laradockm
 docker-compose up -d apache2 mysql elasticsearch
 docker-compose exec workspace composer install
-docker-compose exec workspace composer dump-autoload
 docker-compose exec workspace php artisan migrate
