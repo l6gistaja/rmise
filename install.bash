47bb2962fbdb5347645caef9bae0813d1975b958
Juks <juks@alkohol.ee>
2021-02-23 21:34:39 +0200

    installer improvement

--- install.bash
+++ install.bash
@@ -5,5 +5,5 @@ cd laradock
 cp env-example .env
 docker-compose up -d apache2 mysql elasticsearch
 docker-compose exec workspace composer install
-docker-compose exec workspace composer auto-dump
+docker-compose exec workspace composer dump-autoload
 docker-compose exec workspace php artisan migrate
