cf180e6f9f55c56d0945287347d2b832a6ad5f67
Juks <juks@alkohol.ee>
2021-02-23 22:05:52 +0200

    installer changes

--- install.bash
+++ install.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd laradockm
+cd laradocks
 docker-compose up -d apache2 mysql elasticsearch
 docker-compose exec workspace composer install
 docker-compose exec workspace php artisan migrate
